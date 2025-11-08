2388feef1548af8560a0176c0e27308ee8957981
Thange Sarthak Somnath <sarthakthange1904@gmail.com>
2025-11-08 16:42:44 +0530

    Add factorial functions in fpl_tut_factorial.c
    Implement recursive and iterative factorial functions.

---
+++ fpl_tut_factorial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+double fac_rec(int i){
+    if (i == 0 || i ==1){
+        return 1;
+    }
+    else{
+        return i*fac_rec(i-1);
+    }
+}
+
+double fac_itt(int *i){
+    double fac = 1;
+    for(; *i > 0 ; (*i)--){
+        fac *= (*i);
+    }
+    return fac;
+}
+
+int main(){
+    int num;
+    printf("Enter no to test factorial with recursive function : ");
+    scanf("%d", &num);
+    printf("%0.3lf\n", fac_rec(num));
+    printf("Enter no to test factorial with itterative function : ");
+    scanf("%d", &num);
+    printf("%0.3lf", fac_itt(&num));
+    return 0;
+}
