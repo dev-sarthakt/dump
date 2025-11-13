fd09324445fb718973cad6a6f9576a26f894472d
Thange Sarthak Somnath <sarthakthange1904@gmail.com>
2025-11-13 16:56:22 +0530

    Update eel_4.c

--- eel_4.c
+++ eel_4.c
@@ -19,12 +19,23 @@ void bubble(int entry, struct product *products[]){
     {
         for (int j = i+1; j < entry; j++)
         {
-            if (products[i]->exp <= products[j]->exp)
+            if (products[i]->exp > products[j]->exp)
             {
                 struct product temp;
                 temp = *products[i];
                 *products[i] = *products[j];
                 *products[j] = temp;
+            }
+            else if (products[i]->exp == products[j]->exp)
+            {
+                if (products[i]->total < products[j]->total)
+                {
+                    struct product temp;
+                    temp = *products[i];
+                    *products[i] = *products[j];
+                    *products[j] = temp;
+                }
+                
             }
             
         }
@@ -34,12 +45,13 @@ void bubble(int entry, struct product *products[]){
 }
 
 int main(){
-    int i = 0;
+    int i = 0, sort = 0, ch;
     char add;
     struct product p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, *products[SIZE] = {&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7, &p8, &p9}; 
     while (TRUE)
     {
-        printf("Add new product in database? [Y/N] : ");
+        
+        printf("\nAdd new product in database? [Y/N] : ");
         scanf("%c", &add);
         if (add == 'Y' || add == 'y')
         {
@@ -47,7 +59,7 @@ int main(){
             {
                 return 1;
             }
-            printf("Enter product name : ");
+            printf("\nEnter product name : ");
             scanf("%s", products[i]->name);
             printf("Enter QTY in (kg) : ");
             scanf("%lf", &(products[i]->qty));
@@ -56,15 +68,26 @@ int main(){
             calc_total(i, SIZE, products);
             printf("Enter expiry in days : ");
             scanf("%d", &(products[i]->exp));
-            i++;
+            while ((ch = getchar()) != '\n' && ch != EOF);
+            sort = 1, i++;
         }
-        else{
-            bubble(i, products);
-            for (int g = 0; g < i; g++)
+        else if (add == 'N' || add == 'n'){
+            while ((ch = getchar()) != '\n' && ch != EOF);
+            if (sort == 1)
             {
-                printf("\n%d. %s %lf %d\n", g+1, products[g]->name, products[g]->total, products[g]->exp);
+                bubble(i, products);
+                for (int g = 0; g < i; g++)
+                {
+                    printf("\n| %d | %s | Investment : %lf | Exp : %d |\n", g+1, products[g]->name, products[g]->total, products[g]->exp);
+                }
+                sort = 0;
             }
-            
+            else{
+                for (int g = 0; g < i; g++)
+                {
+                    printf("\n| %d | %s | Investment : %lf | Exp : %d |\n", g+1, products[g]->name, products[g]->total, products[g]->exp);
+                }
+            }
         }
     }
     
